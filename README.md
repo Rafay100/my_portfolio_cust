20314d8607731f4638a74545eaf7137422a5bbb9
Khizra098 <155907425+Khizra098@users.noreply.github.com>
2024-11-10 10:21:06 -0800

    push my porfolio

--- README.md
+++ README.md
@@ -41,3 +41,4 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/bui
 "# portfolio2" 
 "# portfolio.css" 
 "# Portfolio-2" 
+"# portfolio2" 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,38 +1,55 @@
-import React from 'react'
-import Image from 'next/image'; 
+// 
+
+"use client"
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 import '../app/style/card.css';
 
-interface propsType{
-    title: string;
-    desc: string;
-    img: string;
-    tags: string[];
+interface propsType {
+  title: string;
+  desc: string;
+  img: string;
+  tags: string[];
 }
 
-const Card: React.FC <propsType> = ({title, desc, img, tags}) => {
+const Card: React.FC<propsType> = ({ title, desc, img, tags }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 640);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className={`card ${window.innerWidth >= 640 ? 'card-sm' : ''}`} data-aos="zoom-in-up">
+    <div className={`card ${isMobile ? 'card-sm' : ''}`} data-aos="zoom-in-up">
       <div>
-        <Image className= {`card-image ${window.innerWidth >= 640 ? 'card-image-sm' : ''}`}
-        src={img}
-        width={350}
-        height={350}
-        alt={title}
-        />     
+        <Image
+          className={`card-image ${isMobile ? 'card-image-sm' : ''}`}
+          src={img}
+          width={350}
+          height={350}
+          alt={title}
+        />
+      </div>
+      <div className="card-content">
+        <div className="card-title">{title}</div>
+        <div>{desc}</div>
+        <div>
+          {tags.map((el) => (
+            <div key={el}>{el}</div>
+          ))}
         </div>
-        <div className='card-content'>
-            <div className='card-title'>{title}
-                <div>{desc}</div>
-                <div>
-                    {tags.map((el) => (
-                        <div className='card-tags' key={el}>
-                        {el}
-                        </div>))}
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
